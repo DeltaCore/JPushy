83a14c2aa2de4a1bda7ae18332ca115de4d7e319
Marcel Benning <marcel1998@gmx.de>
2013-10-05 21:52:09 +0200

    Update Commit. Level Editor now fully Working.

---
+++ JPushy/common/JPushy/Items.java
@@ -0,0 +1,56 @@
+package JPushy;
+
+import java.util.ArrayList;
+
+import JPushy.Types.Items.Item;
+import JPushy.Types.Items.ItemKey;
+
+public class Items {
+
+public static ArrayList<Item> itemRegistry = new ArrayList<Item>();
+	
+	public static final int maxItems = 32;
+	
+	public static final Item noitem = new Item("noitem", -1, PictureLoader.loadImageFromFile("base.png"));
+	public static final Item key = new ItemKey("Key", 0, PictureLoader.loadImageFromFile("item.key.png"));
+	
+	public static void registerItem(Item t){
+		if(!checkItemId(t.getId())){
+			itemRegistry.add(t);
+			System.out.println("[BlockRegistry] New Item : " + t.getName() + ":" + t.getId());
+		}else{
+			System.out.println("Could'n register Item : " + t.getName() + " with id : " + t.getId());
+		}
+	}
+	
+	private static boolean checkItemId(int id){
+		boolean flag = false;
+		for(int i=0;i<itemRegistry.size();i++){
+			if(itemRegistry.get(i) == null){
+			}else{
+				if(itemRegistry.get(i).getId() == id){
+					flag = true;
+					break;
+				}
+			}
+		}
+		return flag;
+	}
+	
+	public static Item getItemById(int id){
+		for(int i = 0;i<itemRegistry.size();i++){
+			if(itemRegistry.get(i) == null){
+			}else{
+				if(itemRegistry.get(i).getId() == id){
+					return itemRegistry.get(i);
+				}
+			}
+		}
+		return null;
+	}
+	
+	public static void wakeUpDummy(){
+		
+	}
+	
+}
---
+++ JPushy/common/JPushy/Types/Items/ItemKey.java
@@ -0,0 +1,11 @@
+package JPushy.Types.Items;
+
+import JPushy.Types.Picture;
+
+public class ItemKey extends Item {
+	
+	public ItemKey(String name, int id, Picture texture) {
+		super(name, id, texture);
+	}
+	
+}
